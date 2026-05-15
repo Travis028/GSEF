b26e746b4e3e64860a3758037903934731b3f7c5
Travis028 <waynetavis123@gmail.com>
2026-05-15 03:10:23 +0300

    feat: add missing ticket_type and registration database models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,7 @@
-﻿from app.models.base import BaseModel
+﻿from app.models.base import Base, BaseModel
 from app.models.user import User
+from app.models.event import Event
+from app.models.ticket_type import TicketType
+from app.models.registration import Registration
 
-__all__ = ["BaseModel", "User"]
+__all__ = ["Base", "BaseModel", "User", "Event", "TicketType", "Registration"]
